7e5ed99a2fcf62918a38cc4f3b79f0674ad0033f
gdriveramiller <d.riveramiller@gmail.com>
2015-04-29 14:41:31 -0500

    Tower fire methods
    Added firing method to tower script

--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -6,13 +6,52 @@ public class Tower : TowerSuperClass {
 	private Color alphaRed;
 	private Color alphaGreen;
 
+	public bool towerPlaced = false;
+	public GameObject projectile;
+	public GameObject target;
+	public GameObject p;
+	public float fireRate;
+	public float projPos, tarPos, curPos;
+
 	void Awake () {
 		alphaRed = new Color (1, 0, 0, .5f);
 		alphaGreen = new Color (0, 1, 0, .5f);
 	}
 	
 	void Update () {
-	
+		target = getTarget();
+		if(target != null){
+			InvokeRepeating("fire", 1, 2);
+		}
+	}
+
+	public void fire(){
+		Vector3 pos = transform.position + new Vector3(-0.5f, 2.5f, 0);
+		Instantiate (projectile, pos, Quaternion.identity);
+
+		/*projPos = projectile.transform.position;
+		p1 = poi.transform.position;
+		
+		p01 = (1-u)* p0 + u * p1;
+		
+		go.transform.position = p01;*/
+	}
+
+	public GameObject getTarget(){
+		GameObject[] enemies;
+		GameObject closest = null;
+		enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		float distance = Mathf.Infinity;
+		Vector3 position = transform.position;
+		foreach(GameObject enemy in enemies){
+			Vector3 diff = enemy.transform.position - position;
+			float curDistance = diff.sqrMagnitude;
+			if(curDistance < distance){
+				closest = enemy;
+				distance = curDistance;
+			}
+		}
+		return closest;
 	}
 
 	public void makeTransparent () {
